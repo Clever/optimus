643433ff180d7236cfa43a97e67497dc4ea7ac9b
Alex Zylman <alex.zylman@getclever.com>
2014-05-26 14:40:01 -0500

    clarify documentation

--- getl.go
+++ getl.go
@@ -2,14 +2,14 @@ package getl
 
 // Table is a representation of a table of data.
 type Table interface {
-	// Rows returns a channel that provides the rows.
+	// Rows returns a channel that provides the Rows in the table.
 	Rows() <-chan Row
 	// Err returns the first non-EOF error that was encountered by the Table.
 	Err() error
 	// Stop signifies that a Table should stop sending Rows down its channel.
 	// A Table is also responsible for calling Stop on any upstream Tables it knows about.
 	// Stop should be idempotent. It's expected that Stop will never be called by a consumer of a
-	// Table, unless that consumer is also a Table. It can be used to Stop all upstream Tables in
+	// Table unless that consumer is also a Table. It can be used to Stop all upstream Tables in
 	// the event of an error that needs to halt the pipeline.
 	Stop()
 }
@@ -17,8 +17,10 @@ type Table interface {
 // Row is a representation of a line of data in a Table.
 type Row map[string]interface{}
 
-// TransformFunc is a function that can be applied to a Table to transform it.
-type TransformFunc func(<-chan Row, chan<- Row) error
+// TransformFunc is a function that can be applied to a Table to transform it. It should receive the
+// Rows from in and may send any number of Rows to out. It should not return until it has finished
+// all work (received all the Rows it's going to receive, sent all the Rows it's going to send).
+type TransformFunc func(in <-chan Row, out chan<- Row) error
 
 // Transform returns a new Table that provides all the Rows of the input Table transformed with the TransformFunc.
 func Transform(source Table, transform TransformFunc) Table {

3eb48c9b5d11e96a6a9ad0dbf1aeecc010c0714c
Alex Zylman <alex.zylman@clever.com>
2014-07-25 12:49:36 -0700

    sources: add gearman source

---
+++ sources/gearman/gearman_test.go
@@ -0,0 +1,81 @@
+package gearman
+
+import (
+	"fmt"
+	"github.com/Clever/gearman/job"
+	"github.com/Clever/gearman/packet"
+	"github.com/azylman/optimus"
+	"github.com/azylman/optimus/tests"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"io"
+	"testing"
+)
+
+type mockClient struct {
+	*mock.Mock
+	chans []chan *packet.Packet
+}
+
+func (c *mockClient) Close() error {
+	return nil
+}
+
+func (c *mockClient) Submit(fn string, payload []byte, data, warnings io.ReadWriter) (job.Job, error) {
+	_ = c.Mock.Called(fn, payload, data, warnings)
+	packetChan := make(chan *packet.Packet)
+	c.chans = append(c.chans, packetChan)
+	j := job.New("", data, warnings, packetChan)
+	return j, nil
+}
+
+func handlePacket(handle string, kind int, arguments [][]byte) *packet.Packet {
+	if arguments == nil {
+		arguments = [][]byte{}
+	}
+	arguments = append([][]byte{[]byte(handle)}, arguments...)
+	return &packet.Packet{
+		Type:      packet.PacketType(kind),
+		Arguments: arguments,
+	}
+}
+
+func TestGearmanSource(t *testing.T) {
+	c := &mockClient{Mock: &mock.Mock{}, chans: []chan *packet.Packet{}}
+	c.On("Submit", "function", []byte("workload"), mock.Anything, nil).Return(nil, nil).Once()
+	numCalls := 0
+	table := New(c, "function", []byte("workload"), func(in []byte) (optimus.Row, error) {
+		numCalls++
+		assert.Equal(t, in, []byte(fmt.Sprintf("%d", numCalls)))
+		return optimus.Row{"field1": fmt.Sprintf("value%d", numCalls)}, nil
+	})
+	expected := []optimus.Row{
+		{"field1": "value1"},
+		{"field1": "value2"},
+	}
+	go func() {
+		packets := c.chans[0]
+		packets <- handlePacket("", packet.WorkData, [][]byte{[]byte("1")})
+		packets <- handlePacket("", packet.WorkData, [][]byte{[]byte("2")})
+		packets <- handlePacket("", packet.WorkComplete, nil)
+	}()
+	assert.Equal(t, expected, tests.GetRows(table))
+	assert.Nil(t, table.Err())
+}
+
+func TestGearmanSourceFail(t *testing.T) {
+	c := &mockClient{Mock: &mock.Mock{}, chans: []chan *packet.Packet{}}
+	c.On("Submit", "function", []byte("workload"), mock.Anything, nil).Return(nil, nil).Once()
+	table := New(c, "function", []byte("workload"), func(in []byte) (optimus.Row, error) {
+		t.Fatal("never expected converter to be called")
+		return nil, nil
+	})
+	go func() {
+		packets := c.chans[0]
+		packets <- handlePacket("", packet.WorkWarning, [][]byte{[]byte("1")})
+		packets <- handlePacket("", packet.WorkFail, nil)
+	}()
+	expected := []optimus.Row{}
+	assert.Equal(t, expected, tests.GetRows(table))
+	assert.EqualError(t, table.Err(), "gearman job 'function' failed with warnings: 1")
+}

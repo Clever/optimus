5879fceca43d79b0deae44909db49c5da4d67ab2
Alex Zylman <alex.zylman@clever.com>
2015-01-23 10:53:52 -0800

    transforms: simplify pair, add comments

--- transforms/pair.go
+++ transforms/pair.go
@@ -35,48 +35,35 @@ const (
 // on the given hashing functions and join type.
 func Pair(rightTable optimus.Table, leftHash, rightHash RowHasher, join PairType) optimus.TransformFunc {
 	return func(in <-chan optimus.Row, out chan<- optimus.Row) error {
+		// Hash of everything in the right table
 		right := make(map[interface{}][]optimus.Row)
-		found := make(map[interface{}]bool)
-		unfilteredOut := make(chan optimus.Row)
+		// Track whether or not rows in the right table were joined against
+		joined := make(map[interface{}]bool)
+		// The channel of paired rows from the left and right tables
+		pairedRows := make(chan optimus.Row)
+
+		// Build the hash for the right table
+		for row := range rightTable.Rows() {
+			hash, err := rightHash(row)
+			if err != nil {
+				return err
+			}
+			if val := right[hash]; val == nil {
+				right[hash] = []optimus.Row{}
+				joined[hash] = false
+			}
+			right[hash] = append(right[hash], row)
+		}
+		if err := rightTable.Err(); err != nil {
+			return rightTable.Err()
+		}
 
 		wg := errgroup.Group{}
-		wg.Add(2)
-		// Build hash from right table
-		doneRight := make(chan error)
+		// Pair the left table with the right table based on the hashes
+		wg.Add(1)
 		go func() {
-			defer close(doneRight)
+			defer close(pairedRows)
 			defer wg.Done()
-			for row := range rightTable.Rows() {
-				hash, err := rightHash(row)
-				if err != nil {
-					wg.Error(err)
-					doneRight <- err
-					return
-				}
-				// Initialize if dne
-				if val := right[hash]; val == nil {
-					right[hash] = []optimus.Row{}
-					found[hash] = false
-				}
-				right[hash] = append(right[hash], row)
-			}
-			if err := rightTable.Err(); err != nil {
-				wg.Error(err)
-				doneRight <- err
-				return
-			}
-		}()
-
-		// Function that pairs everything in the in channel with the right table. Outputs rows in the
-		// form {"left": leftRow, "right": rightRow}.
-		// Sends everything, with no concern for join type - that's handled later.
-		go func() {
-			defer close(unfilteredOut)
-			defer wg.Done()
-			// Wait until we're done building right hash
-			if err := <-doneRight; err != nil {
-				return
-			}
 
 			for leftRow := range in {
 				hash, err := leftHash(leftRow)
@@ -85,45 +72,55 @@ func Pair(rightTable optimus.Table, leftHash, rightHash RowHasher, join PairType
 					return
 				}
 				if rightRows := right[hash]; rightRows != nil && hash != nil {
-					found[hash] = true
+					joined[hash] = true
 					for _, rightRow := range rightRows {
-						unfilteredOut <- optimus.Row{"left": leftRow, "right": rightRow}
+						pairedRows <- optimus.Row{"left": leftRow, "right": rightRow}
 					}
 				} else {
-					unfilteredOut <- optimus.Row{"left": leftRow}
+					pairedRows <- optimus.Row{"left": leftRow}
 				}
 			}
 
-			for hash, found := range found {
-				if found {
+			for hash, joined := range joined {
+				if joined {
 					continue
 				}
 				for _, rightRow := range right[hash] {
-					unfilteredOut <- optimus.Row{"right": rightRow}
+					pairedRows <- optimus.Row{"right": rightRow}
 				}
 			}
 			return
 		}()
 
-		// Filtered the paired rows based on join type.
-		for row := range unfilteredOut {
-			switch join {
-			case OuterJoin:
-				out <- row
-			case InnerJoin:
-				if row["right"] != nil && row["left"] != nil {
-					out <- row
-				}
-			case LeftJoin:
-				if row["left"] != nil {
-					out <- row
-				}
-			case RightJoin:
-				if row["right"] != nil {
-					out <- row
+		// Filter the paired rows based on our join type
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mustHave := func(keys ...string) func(optimus.Row) (bool, error) {
+				return func(row optimus.Row) (bool, error) {
+					for _, key := range keys {
+						if row[key] == nil {
+							return false, nil
+						}
+					}
+					return true, nil
 				}
 			}
-		}
+			var filter func(optimus.Row) (bool, error)
+			switch join {
+			case OuterJoin:
+				filter = mustHave()
+			case InnerJoin:
+				filter = mustHave("right", "left")
+			case LeftJoin:
+				filter = mustHave("left")
+			case RightJoin:
+				filter = mustHave("right")
+			}
+			if err := Select(filter)(pairedRows, out); err != nil {
+				wg.Error(err)
+			}
+		}()
 		return wg.Wait()
 	}
 }

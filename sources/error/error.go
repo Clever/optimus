ed9bb8ccaf9c8f8edf9ceb9fb7036f279b63dfb5
Josh Deare <dearej@onid.orst.edu>
2015-09-19 13:58:33 -0700

    add comments to exported functions

--- sources/error/error.go
+++ sources/error/error.go
@@ -4,20 +4,27 @@ import (
 	"gopkg.in/Clever/optimus.v3"
 )
 
+// ErrorTable implemements an Optimus Table
+// It's purpose is to return a given error
 type ErrorTable struct {
 	rows    chan optimus.Row
 	err     error
 	Stopped bool
 }
 
+// Err returns an ErrorTable's Error
 func (e *ErrorTable) Err() error {
 	return e.err
 }
 
+// Rows returns the chan for an ErrorTable's Rows
+// note this should only return an error
 func (e *ErrorTable) Rows() <-chan optimus.Row {
 	return e.rows
 }
 
+// Stop fulfills the requirement for ErrorTable
+// to implement the Stop function of an Optimus Table
 func (e *ErrorTable) Stop() {
 	e.Stopped = true
 }

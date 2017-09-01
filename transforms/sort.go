8e80f870b9e713ce5620b24761bb23d1fefda61b
Nate Brennand <nate.brennand@clever.com>
2017-09-01 12:10:39 -0700

    add a "compressed" stable sort

--- transforms/sort.go
+++ transforms/sort.go
@@ -1,6 +1,8 @@
 package transforms
 
 import (
+	"encoding/json"
+	"fmt"
 	"sort"
 
 	"gopkg.in/Clever/optimus.v3"
@@ -53,4 +55,76 @@ func Sort(less func(i, j optimus.Row) (bool, error)) optimus.TransformFunc {
 // It outputs the rows in stably sorted order.
 func StableSort(less func(i, j optimus.Row) (bool, error)) optimus.TransformFunc {
 	return sorter(sort.Stable, less)
+}
+
+type compressedRow struct {
+	key  string
+	blob []byte
+}
+
+type compressedSorter struct {
+	rows []compressedRow
+	err  error
+}
+
+func (r *compressedSorter) Len() int           { return len(r.rows) }
+func (r *compressedSorter) Less(i, j int) bool { return r.rows[i].key < r.rows[j].key }
+func (r *compressedSorter) Swap(i, j int)      { r.rows[i], r.rows[j] = r.rows[j], r.rows[i] }
+
+func compressedSort(sorter func(sort.Interface), getKey func(optimus.Row) (string, error)) optimus.TransformFunc {
+	return func(in <-chan optimus.Row, out chan<- optimus.Row) error {
+		rows := &compressedSorter{rows: []compressedRow{}}
+		for row := range in {
+			// obtain the key for the row and retain the key + blob while sorting
+			key, err := getKey(row)
+			if err != nil {
+				return err
+			}
+
+			rowBlob, err := json.Marshal(row)
+			if err != nil {
+				return err
+			}
+
+			rows.rows = append(rows.rows, compressedRow{
+				key:  key,
+				blob: rowBlob,
+			})
+		}
+
+		sorter(rows)
+
+		if rows.err != nil {
+			return rows.err
+		}
+		for _, row := range rows.rows {
+			var r optimus.Row
+			if err := json.Unmarshal(row.blob, &r); err != nil {
+				return err
+			}
+			out <- r
+		}
+		return nil
+	}
+}
+
+// StableCompressedSort takes in a function that reports whether the row i should sort before row j.
+// It outputs the rows in stably sorted order.
+func StableCompressedSort(getKey func(optimus.Row) (string, error)) optimus.TransformFunc {
+	return compressedSort(sort.Stable, getKey)
+}
+
+// GetKey is utility function for Optimus' StableCompressedSort functionality.
+func GetKey(key string) func(optimus.Row) (string, error) {
+	return func(r optimus.Row) (string, error) {
+		// Don't crash on empty objects
+		if r[key] == nil {
+			return "", nil
+		}
+		k, ok := r[key].(string)
+		if !ok {
+			return "", fmt.Errorf("%s wasn't a string, had value: %#v", key, r[key])
+		}
+		return k, nil
+	}
 }
--- transforms/sort_test.go
+++ transforms/sort_test.go
@@ -38,21 +38,53 @@ func byIntKey(key string) func(optimus.Row, optimus.Row) (bool, error) {
 	}
 }
 
-var sortTests = []struct {
+type sortTestCase struct {
+	desc          string
 	input, output []optimus.Row
 	less          func(optimus.Row, optimus.Row) (bool, error)
 	err           error
-}{
+}
+
+var successTests = []sortTestCase{
 	{
+		desc:   "letter sorting",
 		input:  []optimus.Row{{"a": "q"}, {"a": "b", "b": "d"}, {"a": "c"}},
 		output: []optimus.Row{{"a": "b", "b": "d"}, {"a": "c"}, {"a": "q"}},
 		less:   byStringKey("a"),
 	},
 	{
-		input:  []optimus.Row{{"a": 4}, {"a": 2, "b": "d"}, {"a": 1}},
-		output: []optimus.Row{{"a": 1}, {"a": 2, "b": "d"}, {"a": 4}},
-		less:   byIntKey("a"),
+		desc:   "number sorting",
+		input:  []optimus.Row{{"a": "4"}, {"a": "2", "b": "d"}, {"a": "1"}},
+		output: []optimus.Row{{"a": "1"}, {"a": "2", "b": "d"}, {"a": "4"}},
+		less:   byStringKey("a"),
 	},
+}
+
+func TestSort(t *testing.T) {
+	for _, sortTest := range successTests {
+		for _, sort := range []struct {
+			desc   string
+			sorter optimus.TransformFunc
+		}{
+			{desc: "regular sort", sorter: Sort(sortTest.less)},
+			{desc: "stable sort", sorter: StableSort(sortTest.less)},
+			{desc: "compressed stable sort", sorter: StableCompressedSort(GetKey("a"))},
+		} {
+			t.Run(sortTest.desc, func(t *testing.T) {
+				t.Run(sort.desc, func(t *testing.T) {
+					input := slice.New(sortTest.input)
+					table := optimus.Transform(input, sort.sorter)
+
+					actual := tests.GetRows(table)
+					assert.Equal(t, actual, sortTest.output)
+					assert.NoError(t, table.Err())
+				})
+			})
+		}
+	}
+}
+
+var errorTests = []sortTestCase{
 	{
 		input:  []optimus.Row{{"a": "4"}, {"a": "4", "b": "4"}},
 		output: []optimus.Row{},
@@ -61,56 +93,59 @@ var sortTests = []struct {
 	},
 }
 
-func TestSort(t *testing.T) {
-	for _, sortTest := range sortTests {
-		for _, sort := range []optimus.TransformFunc{Sort(sortTest.less), StableSort(sortTest.less)} {
+func TestSortError(t *testing.T) {
+	for _, sortTest := range errorTests {
+		for _, sort := range []optimus.TransformFunc{
+			Sort(sortTest.less),
+			StableSort(sortTest.less),
+		} {
 			input := slice.New(sortTest.input)
 			table := optimus.Transform(input, sort)
-
-			actual := tests.GetRows(table)
-
-			if sortTest.err != nil {
-				assert.Equal(t, sortTest.err, table.Err())
-			} else {
-				assert.Equal(t, actual, sortTest.output)
-				assert.Nil(t, table.Err())
-			}
+			tests.GetRows(table)
+			assert.Equal(t, sortTest.err, table.Err())
 		}
 	}
 }
 
-func TestStable(t *testing.T) {
-	input := []optimus.Row{
-		{"c": "a", "b": "a"},
-		{"c": "a", "b": "b"},
-		{"c": "a", "b": "c"},
-		{"c": "a", "b": "d"},
-		{"c": "a", "b": "e"},
-		{"c": "a", "b": "f"},
-		{"c": "a", "b": "g"},
-		{"c": "a", "b": "h"},
-		{"c": "a", "b": "i"},
-		{"c": "a", "b": "j"},
-		{"c": "a", "b": "k"},
-		{"c": "a", "b": "l"},
-		{"c": "a", "b": "m"},
-		{"c": "a", "b": "n"},
-		{"c": "a", "b": "o"},
-		{"c": "a", "b": "p"},
-		{"c": "a", "b": "q"},
-		{"c": "a", "b": "r"},
-		{"c": "a", "b": "s"},
-		{"c": "a", "b": "t"},
-		{"c": "a", "b": "u"},
-		{"c": "a", "b": "v"},
-		{"c": "a", "b": "w"},
-		{"c": "a", "b": "x"},
-		{"c": "a", "b": "y"},
-		{"c": "a", "b": "z"},
-	}
+var stableInput = []optimus.Row{
+	{"c": "a", "b": "a"},
+	{"c": "a", "b": "b"},
+	{"c": "a", "b": "c"},
+	{"c": "a", "b": "d"},
+	{"c": "a", "b": "e"},
+	{"c": "a", "b": "f"},
+	{"c": "a", "b": "g"},
+	{"c": "a", "b": "h"},
+	{"c": "a", "b": "i"},
+	{"c": "a", "b": "j"},
+	{"c": "a", "b": "k"},
+	{"c": "a", "b": "l"},
+	{"c": "a", "b": "m"},
+	{"c": "a", "b": "n"},
+	{"c": "a", "b": "o"},
+	{"c": "a", "b": "p"},
+	{"c": "a", "b": "q"},
+	{"c": "a", "b": "r"},
+	{"c": "a", "b": "s"},
+	{"c": "a", "b": "t"},
+	{"c": "a", "b": "u"},
+	{"c": "a", "b": "v"},
+	{"c": "a", "b": "w"},
+	{"c": "a", "b": "x"},
+	{"c": "a", "b": "y"},
+	{"c": "a", "b": "z"},
+}
 
-	table := optimus.Transform(slice.New(input), StableSort(byStringKey("c")))
+func TestStable(t *testing.T) {
+	table := optimus.Transform(slice.New(stableInput), StableSort(byStringKey("c")))
 	actual := tests.GetRows(table)
 	assert.Nil(t, table.Err())
-	assert.Equal(t, actual, input)
+	assert.Equal(t, actual, stableInput)
+}
+
+func TestStableCompressed(t *testing.T) {
+	table := optimus.Transform(slice.New(stableInput), StableCompressedSort(GetKey("c")))
+	actual := tests.GetRows(table)
+	assert.Nil(t, table.Err())
+	assert.Equal(t, actual, stableInput)
 }
